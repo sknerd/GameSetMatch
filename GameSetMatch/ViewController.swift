f37e48437157573d24d988aed16f32edeef273b0
Renald <renksss@gmail.com>
2019-11-13 11:24:07 +0300

    Initial Commit

---
+++ GameSetMatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameSetMatch
+//
+//  Created by renks on 13/11/2019.
+//  Copyright © 2019 Renald Renks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
